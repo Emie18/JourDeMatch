d032a1e86cd231e8c734e76d2da0d05eabf35b61
Emie18 <lalala.emiliat@gmail.com>
2022-06-17 09:30:42 +0200

    ajouter match

--- js/connexion.js
+++ js/connexion.js
@@ -1,12 +1,25 @@
 'use strict';
 
-let bouton = document.getElementById('btn');
-let tt = document.getElementById('nom');
-bouton.addEventListener('click', event => {
-  ajaxRequest('GET', '../php/request.php/profil', affiche);
-});
+$(document).ready(function(){
+    $("#loginform").on("submit", function(e){
+      e.preventDefault();
 
-function affiche(data){
-  console.log(data);
-  tt.innerHTML = data[0].email;
-}
+      var email = $("#email").val();
+      var password = $("#password").val();
+
+        $.ajax({
+            url : "../php/login.php",
+            type:"POST",
+            cache:false,
+            data:{email:email,password:password},
+            success:function(response){
+                if(response == '1') {
+                    window.location.replace("profile.php");
+                } else if(response == '0') {
+                    $(".show-message").show();
+                    $(".ajax-message").text('Email or Password is Invalid');
+                }
+            }
+        });
+    });
+});
