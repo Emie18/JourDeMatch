4f1925e5b2e6a01a4c6eab7584f49e989614be62
Emie18 <lalala.emiliat@gmail.com>
2022-06-19 11:56:40 +0200

    css

--- js/notif.js
+++ js/notif.js
@@ -2,8 +2,18 @@ const button = document.getElementById('notif');
 let block = document.getElementById('affiche_notif');
 button.addEventListener('click', event => {
     //console.log(event);
-    $('#affiche_notif').toggle(1000, "swing");
+    //$('#affiche_notif').toggle(1000, "swing");
+    //$('#affiche_notif').toggle('active');
+    document.getElementById('affiche_notif').classList.toggle('active');
+   
 });
+let button2 = document.getElementById('notif2');
+button2.addEventListener('click', event => {
+    console.log('oui!');
+    $('#affiche_notif').toggle(1000, "swing");
+
+});
+
 setInterval(ajaxRequest, 5000, 'GET', 'php/request.php/recherche_notif/', affiche);
 //ajaxRequest('GET','php/request.php/recherche_notif/',affiche)
 let text = "";
@@ -11,6 +21,7 @@ function affiche(data) {
     $('#affiche_notif').html('');
     if (data != "") {
         document.getElementById('icone_notif').src = "icone/notif_oui.png";
+        document.getElementById('icone_notif2').src = "icone/notif_oui.png";
     }
     data.forEach(elem => {
         let p = document.createElement('p');
